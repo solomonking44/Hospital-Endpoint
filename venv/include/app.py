881925142db2cc10d573a81c85f784e79fca3ff3
solomonking44 <mugishasolomon520@gmail.com>
2023-04-05 09:28:10 +0300

    New XML file created

--- venv/include/app.py
+++ venv/include/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 import json
+from xml.dom import minidom
 
 #creating the flask app
 app = Flask(__name__)
@@ -22,10 +23,15 @@ class GrandOak(Resource):
 class PineValley(Resource):
     def get(self):
         return PINE_VALLEY
+#xml resource
+class XmlFile(Resource):
+    def get(self):
+        return xml_data
 
 #adding resources to the API
 api.add_resource(GrandOak, '/grandoak')
 api.add_resource(PineValley, '/pinevalley')
+api.add_resource(XmlFile, '/xmlfile')
 
 #grand oaks data source
 grandoak = open('grandoak.json')
@@ -35,6 +41,10 @@ GRAND_OAKS = json.load(grandoak)
 pinevalley = open('pinevalley.json')
 PINE_VALLEY = json.load(pinevalley)
 
+#importing xml file
+#xml_file = minidom.parse('file.xml')
+#getting elementbytag
+#xml_data = xml_file.getElementsByTagName('request')
 
 if __name__ == '__main__':
     app.run(debug=True)
