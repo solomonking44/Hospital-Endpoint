cc392ac853805c89caf80e4e2aa45e52fc1290e1
solomonking44 <mugishasolomon520@gmail.com>
2023-04-24 23:40:33 +0300

    "New endpoints"

--- venv/include/app.py
+++ venv/include/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_restful import Api, Resource
 import json
-from xml.dom import minidom
+#from xml.dom import minidom
 
 #creating the flask app
 app = Flask(__name__)
@@ -24,14 +24,33 @@ class PineValley(Resource):
     def get(self):
         return PINE_VALLEY
 #xml resource
-class XmlFile(Resource):
+#class XmlFile(Resource):
+ #   def get(self):
+  #      return XML_DATA
+
+#data mapper json
+class DataMapper(Resource):
     def get(self):
-        return xml_data
+        return DATA_MAPPER_INPUT
+
+
+#all hospital records of doctors
+class Hospital(Resource):
+    def get(self):
+        return HOSPITAL_RECORDS
+
+#complex data mapper scenario
+class DataMapperComplex(Resource):
+    def get(self):
+        return DATA_MAPPER_COMPLEX
 
 #adding resources to the API
 api.add_resource(GrandOak, '/grandoak')
 api.add_resource(PineValley, '/pinevalley')
-api.add_resource(XmlFile, '/xmlfile')
+#api.add_resource(XmlFile, '/xmlfile')
+api.add_resource(DataMapper, '/datamapper')
+api.add_resource(Hospital, '/hospital')
+api.add_resource(DataMapperComplex, '/datamappercomplex')
 
 #grand oaks data source
 grandoak = open('grandoak.json')
@@ -45,6 +64,19 @@ PINE_VALLEY = json.load(pinevalley)
 #xml_file = minidom.parse('file.xml')
 #getting elementbytag
 #xml_data = xml_file.getElementsByTagName('request')
+
+
+#data mapper json input
+data_mapper_input = open('dataMapperInput.json')
+DATA_MAPPER_INPUT = json.load(data_mapper_input)
+
+#hospital json file
+hospital_file = open('hospital.json')
+HOSPITAL_RECORDS = json.load(hospital_file)
+
+#data mapper complex file
+data_mapper_complex = open('jsonFileTest.json')
+DATA_MAPPER_COMPLEX = json.load(data_mapper_complex)
 
 if __name__ == '__main__':
     app.run(debug=True)
